e5c076b13f1188139dc622da224da651836c60de
Hendrik Lösch <loeschhendrik@googlemail.com>
2023-02-06 15:57:29 +0000

    Created first tests and solution file

--- back-end/Chat.Implementation/Controllers/ChatController.cs
+++ back-end/Chat.Implementation/Controllers/ChatController.cs
@@ -1,6 +1,9 @@
 namespace Chat.Implementation;
 
-public class ChatController 
+public class ChatController
 {
-
+    public IEnumerable<object> GetMessages()
+    {
+        return new List<object> { new object()};
+    }
 }
--- back-end/Chat.Tests/ChatTests.cs
+++ back-end/Chat.Tests/ChatTests.cs
@@ -1,6 +1,7 @@
 namespace Chat.Tests;
 
 using Chat.Implementation;
+using System.Linq;
 
 public class ChatTests
 {
@@ -10,5 +11,15 @@ public class ChatTests
         var sut = new ChatController();
 
         Assert.NotNull(sut);
+    }
+
+    [Fact]
+    public void Empty_list_is_returned_if_no_chat_messages_are_available()
+    {
+        var sut = new ChatController();
+
+        var messages = sut.GetMessages();
+
+        Assert.True(messages.Any());
     }
 }
